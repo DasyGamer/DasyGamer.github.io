b80bd41a23853b6d358909a7ae7714227eec2e99
abbaszamil <78052807+DasyGamer@users.noreply.github.com>
2021-01-26 15:12:33 -0800

    uploaded factfinder

---
+++ factfinder/script.js
@@ -0,0 +1,42 @@
+
+var fb = document.querySelector("#factbox");  
+
+document.querySelector("#factform").Z1.addEventListener("click",Z1);
+
+
+function Z1(){  
+fb.innerHTML = "The hottest spot on the planet is in Libya. Specifically, the hottest spot ever recorded on Earth is El Azizia, in Libya, where a temperature of 136 degrees";
+}
+
+document.querySelector("#factform").Z2.addEventListener("click",Z2 );
+
+function Z2(){ 
+fb.innerHTML = "The Ms in M&Ms stand for Mars and MurrieThat would be Forrest Mars and Bruce Murrie, the two businessmen who created the candy-coated chocolates.";
+
+}
+
+document.querySelector("#factform").Z3.addEventListener("click",Z3 );
+
+function Z3() {
+fb.innerHTML = "Cotton candy was invented by a dentist It is not known whether William Morrison had an ulterior motive for inventing the soft confection, but the dentist no doubt helped ensure others in his profession continued drawing in plenty of customers.<br><img src='images/cottoncandy.jpg' alt='flipping cartoon perogie'>"; 
+
+
+} 
+
+document.querySelector("#factform").Z4.addEventListener("click",Z4 );
+
+function Z4 () { 
+fb.innerHTML = " There Were 10,297 Balloons in pixar cartoon movie Up";
+
+}
+
+document.querySelector("#factform").Z5.addEventListener("click",Z5 );
+
+function Z5 () { 
+fb.innerHTML = "Minecraft unit sales worldwide 2016-2020. Since its release in 2011, Minecraft has recorded worldwide sales of over 200 million units, making it one of the best-selling games of all time along with other legendary games such as Tetris and Grand Theft Auto V.";
+
+}
+
+
+
+
